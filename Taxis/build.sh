0839879ffa27c7cee288553f2942220b518ccb48
saida <goba080428hhgnrla0@soycecytem.mx>
2026-08-04 18:38:13 -0600

    build

--- Taxis/build.sh
+++ Taxis/build.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
-# Detener la ejecución si ocurre cualquier error
 set -o errexit
 
 # 1. Instalar dependencias de Python
 pip install -r requirements.txt
 
-# 2. Entrar a la carpeta frontend, instalar paquetes y compilar React
-cd frontend
+# 2. Subir un nivel para entrar a frontend y compilar React
+cd ../frontend
 npm install
 npm run build
-cd ..
+cd ../Taxis
 
-# 3. Aplicar migraciones en la Base de Datos de Django
+# 3. Aplicar migraciones y recolectar estáticos
 python manage.py migrate
-
-# 4. Recolectar todos los archivos estáticos (incluyendo el build de React)
 python manage.py collectstatic --no-input
