dca6ad922a940ede0629c3d8e156fcafc93bdad3
saida <goba080428hhgnrla0@soycecytem.mx>
2026-08-04 19:53:43 -0600

    Corregida duplicacion de urls.py y excluido /static/ del comodin SPA

--- Taxis/Taxicom/urls.py
+++ Taxis/Taxicom/urls.py
@@ -1,19 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-from Taxis import views, api_views 
-from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin_django/', admin.site.urls), 
-    ##path('', views.home_publico, name='home_publico'),
-    
-    path('api/chofer/modalidad/', api_views.api_cambiar_modalidad_chofer, name='api_cambiar_modalidad'),
-    path('api/viaje/especial/solicitar/', api_views.api_solicitar_viaje_especial, name='api_solicitar_especial'),
-    
-    path('', include('Taxis.urls')),
-    
-    re_path(r'^.*$', TemplateView.as_view(template_name='taxis/react_base.html')),
+    path('admin_django/', admin.site.urls),
++    path('', include('Taxis.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Taxis/Taxis/urls.py
+++ Taxis/Taxis/urls.py
@@ -1,15 +1,9 @@
-from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
-from Taxis import views
-from django.conf.urls.static import static
-from django.conf import settings
-from Taxis import api_views
+from Taxis import views, api_views
 
 urlpatterns = [
-    # Panel de Administración propio de Django
-
-    # APIs PARA APP ANDROID Y SISTEMA DE UBICACIÓN
+    # APIs PARA APP ANDROID Y UBICACIÓN
     path('api/chofer/ubicacion/', views.actualizar_ubicacion_chofer, name='actualizar_ubicacion_chofer'),
     path('api/admin/choferes-mapa/', views.api_choferes_activos_mapa, name='api_choferes_activos_mapa'),
     
@@ -19,12 +13,12 @@ urlpatterns = [
     path('api/chofer/modalidad/', api_views.api_cambiar_modalidad_chofer, name='api_cambiar_modalidad_chofer'),
     path('api/viaje/solicitar/', api_views.api_solicitar_viaje_especial, name='api_solicitar_viaje_especial'),
 
-    # ENDPOINTS API JSON UTILIZADOS POR EL FRONTEND EN REACT
+    # ENDPOINTS API JSON PARA FRONTEND REACT
     path('api/web/login/', views.api_web_login, name='api_web_login'),
     path('api/web/choferes-activos/', views.api_choferes_activos_mapa, name='api_web_choferes_activos_mapa'),
     path('api/web/gestion-choferes/', views.api_gestion_choferes, name='api_gestion_choferes'),
     path('api/web/roles/', views.api_roles, name='api_roles'),
 
-    re_path(r'^.*$', TemplateView.as_view(template_name='taxis/react_base.html')),
+    # ⚠️ REGLA COMODÍN CORREGIDA: Excluye /static/ de React Router
+    re_path(r'^(?!static/).*$', TemplateView.as_view(template_name='taxis/react_base.html')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
