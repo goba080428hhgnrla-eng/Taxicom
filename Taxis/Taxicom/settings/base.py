997af2519eea10dfcd06a13e0d51c3be76ac7f9c
saida <goba080428hhgnrla0@soycecytem.mx>
2026-08-05 19:27:14 -0600

    Corregida mayuscula en la ruta de PerfilUsuarioJWTAuthentication para REST_FRAMEWORK

--- Taxis/Taxicom/settings/base.py
+++ Taxis/Taxicom/settings/base.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "taxis.authentication.PerfilUsuarioJWTAuthentication",
+        "Taxis.authentication.PerfilUsuarioJWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
