0211d2e59a131d824583900baba4b59a9efd8ac6
saida <goba080428hhgnrla0@soycecytem.mx>
2026-08-08 14:14:37 -0600

    correción de asgi

--- Taxis/Taxicom/asgi.py
+++ Taxis/Taxicom/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taxicom.settings')
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import Taxicom.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taxicom.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             Taxicom.routing.websocket_urlpatterns
